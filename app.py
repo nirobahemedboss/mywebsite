1bf7c735c84179025bd773994dc64079e07c4953
nirobahemedboss <mdnirobcomputers46@gmail.com>
2026-06-18 09:38:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,8 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('home'))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5005))
+    app.run(host='0.0.0.0', port=port, debug=False)
